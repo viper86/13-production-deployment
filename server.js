639b3508bc2252d1ae6e8664f9f95628e29ec490
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2016-10-05 13:15:00 -0700

    update for inclass

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express'),
   // TODO: require in our request proxy module
-
+  requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
 
 // TODO: now use our proxy within a function to request
 //        our github data on the server.
+
+function proxyGitHub(request, response) {
+  console.log('Routing Github request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0]
+  }))(request, response);
+};
+
+app.get('/github/*', proxyGitHub);
 
 app.use(express.static('./'));
 
